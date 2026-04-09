7ed6ecea75631fc144b5e05dedc78a18a1c730bc
siddarth182772-cloud <siddarth.182772@gmail.com>
2026-04-09 11:47:32 +0530

    Update app.py

--- Frontend-flask-server/app.py
+++ Frontend-flask-server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
 # Backend API
-BACKEND_API = "http://127.0.0.1:5000"  # or your backend private IP
+BACKEND_API = "http://10.0.13.112:5000"  # or your backend private IP
 
 
 # Home page: show users
